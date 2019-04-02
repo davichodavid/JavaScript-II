8f23ac2fdbbf15efa777920bc91b0d5a1a4b5386
davichodavid <davichodavid@yahoo.com>
2019-04-02 17:42:21 -0400

    Finished MVP for all files, finished stretch for callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', "Pencil", 'Notebook', 'yo-yo', 'Gum'];
 
 /* 
 
@@ -24,31 +24,41 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-  return cb(arr.length);
+  return cb(arr.length)
 }
+
+getLength(items, length => console.log(length));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  return cb(arr.length - 1);
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, last => console.log(last))
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   return cb(x + y);
 }
 
+sumNums(12, 4, callback => console.log(callback));
+
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   return cb(x * y);
 }
 
+multiplyNums(4, 12, callback => console.log(callback));
+
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
+
+contains("banana", items, callback => console.log(callback));
 
 /* STRETCH PROBLEM */
 
@@ -56,4 +66,7 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  return cb(array.filter((item, index) => array.indexOf(item) === index));
 }
+
+removeDuplicates(items, callback => console.log(callback));
